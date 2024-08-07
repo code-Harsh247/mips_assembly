e441eb506fbad91d562d5ef5a16f79d66202815a
code-Harsh247 <justharsh2407@gmail.com>
2024-08-07 12:24:06 +0530

    matrix

--- matrix/matrix.asm
+++ matrix/matrix.asm
@@ -10,64 +10,92 @@ col_count: .word 0
 
 matrix_base: .word 0
 
-total: .word 0
-
 .text
 .globl main 
 
 main:
     # Prompt to enter rows
-    li $v0 4
-    la $a0 prompt_rows
+    li $v0, 4
+    la $a0, prompt_rows
     syscall
 
     # Read number of rows from user
-    li $v0 5
+    li $v0, 5
     syscall
-    la $t1 row_count
-    sw $v0 0($t1)
+    sw $v0, row_count
 
     # Prompt to enter columns
-    li $v0 4
-    la $a0 prompt_cols
+    li $v0, 4
+    la $a0, prompt_cols
     syscall
 
-    # Read number of rows from user
-    li $v0 5
+    # Read number of columns from user
+    li $v0, 5
     syscall
-    la $t1 col_count
-    sw $v0 0($t1)
+    sw $v0, col_count
 
-    lw $s0 row_count
-    lw $s1 col_count
-    mul $s2 $s0 $s1 #total in s2
-    
-    li $v0 9
-    move $a0 $s2
+    lw $s0, row_count        # Load row count into $s0
+    lw $s1, col_count        # Load column count into $s1
+    mul $s2, $s0, $s1        # Total number of elements in $s2
+
+    # Allocate memory for the matrix
+    li $v0, 9
+    mul $a0, $s2, 4          # Allocate space for 4-byte integers
     syscall
-    sw $v0 matrix_base
+    sw $v0, matrix_base      # Store base address of matrix
 
-    lw $s3 matrix_base # base address
-    move $t7 $s3 # current address 
+    lw $s3, matrix_base      # Load base address of matrix
+    move $t7, $s3            # Set current address for input
 
-    li $t0 0 
+    li $t0, 0                # Initialize element counter
 
 fill_matrix:
-    beq $t0 $s2 print_original
+    beq $t0, $s2, print_original   # If all elements are filled, proceed to print
 
-    div $t0 $s1
-    mflo $t1 # row
-    mfhi $t2 # col
-
-    li $v0 5
+    # Prompt for matrix element input
+    li $v0, 5
     syscall
 
-    sw $v0 0($t7)
+    # Store user input into matrix
+    sw $v0, 0($t7)
 
-    addi $t0 $t0 1
-    addi $t7 $t7 4
+    # Move to the next element
+    addi $t0, $t0, 1
+    addi $t7, $t7, 4
 
     j fill_matrix
 
 print_original:
-    
+    li $t0, 0                # Reset element counter
+    move $t7, $s3            # Reset address pointer to start of matrix
+
+print_matrix:
+    beq $t0, $s2, exit       # If all elements are printed, exit
+
+    lw $a0, 0($t7)           # Load element to print
+    li $v0, 1
+    syscall                  # Print integer
+
+    li $v0, 4
+    la $a0, space
+    syscall                  # Print space
+
+    # Calculate next row and column positions
+    addi $t7, $t7, 4         # Move to the next element
+    addi $t0, $t0, 1         # Increment element counter
+
+    # Check if we need to print a newline
+    div $t0, $s1
+    mfhi $t2                 # Get column index (remainder)
+    bnez $t2, print_matrix   # If not at end of row, continue
+
+    # Print newline at the end of a row
+    li $v0, 4
+    la $a0, newline
+    syscall
+
+    j print_matrix
+
+exit:
+    li $v0, 10
+    syscall
